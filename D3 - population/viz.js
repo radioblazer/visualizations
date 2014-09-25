f7074ccf67392b9b1eb63f7d6c5b1ce1bf0f1e11
Matt Townsend <mtownsend@gatech.edu>
2014-09-25 15:27:35 -0400

    Latest revision

--- D3 - population/viz.js
+++ D3 - population/viz.js
@@ -1,13 +1,14 @@
-
+// Authors: Matthew Townsend and Rahul Singh
 function displayData(year) {
     console.log(year);
     year = parseInt(year);
     console.log(year);
     var series1 = [];
     var series2 = [];
+    var series3 = [];
 
     var year;
-    d3.json("data.json", function(data) {
+    d3.json("population.json", function(data) {
         console.log(data);
         console.log(data.length);
         console.log(data[0]);
@@ -35,8 +36,72 @@ function displayData(year) {
                 values: series2
             }
         ];
-        
+        // sum all populations
+        // check if age is 60 or greater 
+        // add to older_pop sum 
+        // 
+        var totalPopulation = 0;
+        var olderPopulation = 0;
+        var numWorking = 0;
+        var numYounger = 0;
+
+        for(var i = data.length-1; i >= 0; i--) {
+            if (data[i].year === year) {
+                totalPopulation += data[i].people;
+                if (data[i].age >= 60) {
+                    olderPopulation += data[i].people;
+                }
+                if (data[i].age >= 15 && data[i].age < 60) {
+                    numWorking += data[i].people;
+
+                }
+                if(data[i].age < 15) {
+                    numYounger += data[i].people;
+                }
+            } 
+            
+        }
+        var olderPieData = [
+            {
+                "label": "# over 60",
+                "value": parseInt(olderPopulation)
+            } ,
+            {
+                "label": "Everyone else", 
+                "value": parseInt(totalPopulation - olderPopulation)
+            }
+        ]
+
+
+
+        var workingPop = [
+            {
+                "label": "# between 15 and 60",
+                "value": parseInt(numWorking)
+            } ,
+            {
+                "label": "Everyone else", 
+                "value": parseInt(totalPopulation - numWorking)
+            }
+        ]
+
+        var youngerPop = [
+            {
+                "label": "# below 15",
+                "value": parseInt(numYounger)
+            } ,
+            {
+                "label": "Everyone else", 
+                "value": parseInt(totalPopulation - numYounger)
+            }
+        ]
+
+
         renderChart(output);
+        renderOlderPieChart(olderPieData);
+        renderWorkingPieChart(workingPop);
+        renderYoungerPieChart(youngerPop);
+
         $('#centerLabel').text(year);
     });
 
@@ -69,13 +134,90 @@ function renderChart(data) {
                      chart.update();
                  }
              );
-
         return chart;
     });
 }
+
+function renderOlderPieChart(data) {
+    nv.addGraph(function() {
+      var chart = nv.models.pieChart()
+          .x(function(d) { return d.label })
+          .y(function(d) { return d.value })
+          .showLabels(true)
+          .showLegend(false);
+
+        d3.select("#pieChart svg")
+            .datum(data)
+            .transition().duration(350)
+            .call(chart);
+
+      return chart;
+    });
+}
+function renderWorkingPieChart(data) {
+    nv.addGraph(function() {
+      var chart = nv.models.pieChart()
+          .x(function(d) { return d.label })
+          .y(function(d) { return d.value })
+          .showLabels(true)
+          .showLegend(false);
+
+        d3.select("#workingPieChart svg")
+            .datum(data)
+            .transition().duration(350)
+            .call(chart);
+
+      return chart;
+    });
+}
+function renderYoungerPieChart(data) {
+    nv.addGraph(function() {
+      var chart = nv.models.pieChart()
+          .x(function(d) { return d.label })
+          .y(function(d) { return d.value })
+          .showLabels(true)
+          .showLegend(false);
+
+        d3.select("#youngerPieChart svg")
+            .datum(data)
+            .transition().duration(350)
+            .call(chart);
+
+      return chart;
+    });
+}
+
+var timeout;
+var animate = (function () {;
+    var dates = [1850, 1860, 1870, 1880, 
+    1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000];
+    var index = 0;
+    timeout = 1500;
+
+    return function () {
+        if (timeout !== 1000000) {
+            displayData(dates[index]);
+        }
+        index = (index + 1) % dates.length;
+        timeId = setTimeout(animate, timeout);
+    }
+}());
+
+window.onload = animate;
+
+
 $(document).ready(function() {
         $('.btn-default').click(function() {
-            displayData($(this).attr("value"));
+            console.log("hello");
+            if(($(this).attr("value")) === "Stop") {
+                timeout = 1000000;
+            } else if(($(this).attr("value")) === "Play") {
+                location.reload();
+            } else {
+                timeout = 1000000;
+                displayData($(this).attr("value"));
+
+            }
         });
 });
 
